3be2a0b55bee248b45785c4f0c02406f1f373681
Teflon97 <tefelolmak@gmail.com>
2026-05-05 10:44:03 +0200

    app file

--- app.py
+++ app.py
@@ -1,7 +1,4 @@
 from backend.server import app
 
-# This entry point is required for Vercel deployment
-# It exposes the Flask app instance to the Serverless environment
-
 if __name__ == "__main__":
     app.run()
